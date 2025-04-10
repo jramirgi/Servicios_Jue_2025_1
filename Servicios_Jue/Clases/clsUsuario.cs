a44a0038b983ee4b89ea4f2c9815984249751b9f
joseramirez <joseramirez8046@correo.itm.edu.co>
2025-04-10 18:07:55 -0500

    cypher y usuarios

---
+++ Servicios_Jue/Clases/clsUsuario.cs
@@ -0,0 +1,46 @@
+﻿using Servicios_Jue.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Servicios_Jue.Clases
+{
+    public class clsUsuario
+    {
+        private DBSuperEntities dbSuper = new DBSuperEntities();
+        public Usuario usuario { get; set; }
+        public string CrearUsuario(int idPerfil)
+        {
+            try
+            {
+                clsCypher cypher = new clsCypher();
+                cypher.Password = usuario.Clave;
+                if (cypher.CifrarClave())
+                {
+                    //Grabar el usuario, se deben leer los datos de la clase cypher con la información encriptada
+                    usuario.Clave = cypher.PasswordCifrado;
+                    usuario.Salt = cypher.Salt;
+                    dbSuper.Usuarios.Add(usuario);
+                    dbSuper.SaveChanges();
+                    //se debe grabar el perfil del usuario
+                    Usuario_Perfil UsuarioPerfil = new Usuario_Perfil();
+                    UsuarioPerfil.idPerfil = idPerfil;
+                    UsuarioPerfil.Activo = true;
+                    UsuarioPerfil.idUsuario = usuario.id; //El id del Usuario queda grabado en la clase usuario al grabar en la base de datos.
+                    dbSuper.Usuario_Perfil.Add(UsuarioPerfil);
+                    dbSuper.SaveChanges();
+                    return "Se creó el usuario correctamente";
+                }
+                else
+                {
+                    return "No se pudo encriptar la clave del usuario";
+                }
+            }
+            catch(Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+    }
+}
---
+++ Servicios_Jue/Controllers/UsuariosController.cs
@@ -0,0 +1,24 @@
+﻿using Servicios_Jue.Clases;
+using Servicios_Jue.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Servicios_Jue.Controllers
+{
+    [RoutePrefix("api/Usuarios")]
+    public class UsuariosController : ApiController
+    {
+        [HttpPost]
+        [Route("CrearUsuario")]
+        public string CrearUsuario([FromBody] Usuario usuario, int idPerfil)
+        {
+            clsUsuario Usuario = new clsUsuario();
+            Usuario.usuario = usuario;
+            return Usuario.CrearUsuario(idPerfil);
+        }
+    }
+}
